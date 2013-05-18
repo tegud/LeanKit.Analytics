9c37f2f129f8c8aa390a36fc6ddfc003cd7d3f2d
Steve Elliott <steve@tegud.net>
2013-05-18 01:11:03 +0100

    Graph for waste vs blue light
    And responsive design. Because Mobile. Possibly.

---
+++ LeanKit.Analytics/LeanKit.Analytics.Tests/HomeControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using LeanKit.Analytics.Controllers;
+using NUnit.Framework;
+
+namespace LeanKit.Analytics.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void IndexSetsViewToIndex()
+        {
+            Assert.That(new HomeController().Index().ViewName, Is.EqualTo("Index"));
+        }
+    }
+}
