e0088a11c61b64fe08725142cc801e1e661c3d3a
Steve Elliott <steve@tegud.net>
2013-06-03 22:06:46 +0100

    New controller for new releases

---
+++ LeanKit.Analytics/LeanKit.ReleaseManager/Views/NewRelease/NewReleaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace LeanKit.ReleaseManager.Views.NewRelease
+{
+    public class NewReleaseController : Controller
+    {
+        [HttpPost]
+        public RedirectResult Index(NewReleaseViewModel release)
+        {
+            return new RedirectResult("/");
+        }
+    }
+
+    public class NewReleaseViewModel
+    {
+        public DateTime PlannedDate { get; set; }
+
+        public string PlannedTime { get; set; }
+
+        public string SelectedTickets { get; set; }
+    }
+}
