f83abbbb08b63ccb6d6a51a082f3be5d7cd3dd2c
Steve Elliott <steve@tegud.net>
2013-06-06 18:32:03 +0100

    Hardcoding most values in controller instead

--- LeanKit.Analytics/LeanKit.ReleaseManager/Controllers/ReleaseController.cs
+++ LeanKit.Analytics/LeanKit.ReleaseManager/Controllers/ReleaseController.cs
@@ -10,8 +10,64 @@ namespace LeanKit.ReleaseManager.Controllers
     {
         public ActionResult Index(int id)
         {
-            return View();
+            var releaseViewModel = new ReleaseDetailViewModel
+                {
+                    Id = id,
+                    PlannedTime = new ReleasePlannedTime
+                        {
+                            StartFriendlyText = "Today at 10:00",
+                            Duration = 60
+                        },
+                    ActualTime = new ReleaseActualTime
+                        {
+                            StartedFriendlyText = "Today at 10:00",
+                            CompletedFriendlyText = "Today at 10:45",
+                            Duration = 45
+                        },
+                    Status = new ReleaseStatusViewModel
+                        {
+                            Text = "Awaiting Approval"
+                        }
+                };
+            return View("Index", releaseViewModel);
         }
+    }
 
+    public class ReleaseDetailViewModel
+    {
+        public int Id { get; set; }
+
+        public ReleasePlannedTime PlannedTime { get; set; }
+
+        public ReleaseActualTime ActualTime { get; set; }
+
+        public string SvnRevision { get; set; }
+
+        public string ServiceNowId { get; set; }
+
+        public ReleaseStatusViewModel Status { get; set; }
+    }
+
+    public class ReleaseStatusViewModel
+    {
+        public string Text { get; set; }
+
+        public string CssClass { get; set; }
+    }
+
+    public class ReleasePlannedTime
+    {
+        public string StartFriendlyText { get; set; }
+
+        public int Duration { get; set; }
+    }
+
+    public class ReleaseActualTime
+    {
+        public string StartedFriendlyText { get; set; }
+
+        public string CompletedFriendlyText { get; set; }
+
+        public int Duration { get; set; }
     }
 }
