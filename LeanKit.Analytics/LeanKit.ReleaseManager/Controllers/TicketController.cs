eccbbdab0763a2c2a458ee6aa45472a77448481b
Steve Elliott <steve@tegud.net>
2013-06-13 16:04:06 +0100

    Ticket status overview

--- LeanKit.Analytics/LeanKit.ReleaseManager/Controllers/TicketController.cs
+++ LeanKit.Analytics/LeanKit.ReleaseManager/Controllers/TicketController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using LeanKit.Analytics.Models.ViewModels;
 using LeanKit.Data;
 using LeanKit.Data.SQL;
 using LeanKit.Utilities.DateAndTime;
@@ -44,30 +42,60 @@ namespace LeanKit.ReleaseManager.Controllers
 
             var totalActivityHours = (double)allTicketActivityDurations.Sum(t => t.Duration.Hours);
 
-            var wasteGraphList = new List<WasteGraphActivity>
+            var activityBreakdownItems = new List<ActivityBreakdownItem>
                 {
-                    new WasteGraphActivity { Activity = "Developing" },
-                    new WasteGraphActivity { Activity = "Testing" },
-                    new WasteGraphActivity { Activity = "Waiting to Test",IsWaste = true },
-                    new WasteGraphActivity { Activity = "Waiting to Release", IsWaste = true },
-                    new WasteGraphActivity { Activity = "Blocked", IsWaste = true }
+                    new ActivityBreakdownItem { Activity = "Developing" },
+                    new ActivityBreakdownItem { Activity = "Testing" },
+                    new ActivityBreakdownItem { Activity = "Waiting to Test",IsWaste = true },
+                    new ActivityBreakdownItem { Activity = "Waiting to Release", IsWaste = true },
+                    new ActivityBreakdownItem { Activity = "Blocked", IsWaste = true }
                 };
 
             foreach (var ticketActivity in allTicketActivityDurations)
             {
-                var wasteGraphActivity = wasteGraphList.FirstOrDefault(a => a.Activity == ticketActivity.Title);
+                var activity = activityBreakdownItems.FirstOrDefault(a => a.Activity == ticketActivity.Title);
 
-                if (wasteGraphActivity == null)
+                if (activity == null)
                 {
                     continue;
                 }
 
-                wasteGraphActivity.Percent += ((ticketActivity.Duration.Hours / totalActivityHours) * 100);
+                activity.Percent += ((ticketActivity.Duration.Hours / totalActivityHours) * 100);
+                activity.Hours += ticketActivity.Duration.Hours;
+                activity.Days += ticketActivity.Duration.Days;
             }
 
-            var x = new WasteGraph(wasteGraphList);
+            var activityBreakdown = new ActivityBreakdown(activityBreakdownItems);
 
-            return View(x);
+            return View(new TicketViewModel
+                {
+                    ActivityBreakdown = activityBreakdown,
+                    Title = ticket.Title,
+                    ExternalId = ticket.ExternalId,
+                    CurrentStatus = GetTicketCurrentStatus(ticket),
+                    Started = DateFriendlyText(ticket.Started),
+                    Finished = DateFriendlyText(ticket.Finished)
+                });
+        }
+
+        private static string DateFriendlyText(DateTime date)
+        {
+            if (date > DateTime.MinValue)
+            {
+                return date.ToFriendlyText("dd MMM yyyy", " \"at\" HH:mm");
+            }
+
+            return "";
+        }
+
+        private static string GetTicketCurrentStatus(Ticket ticket)
+        {
+            if (ticket.Finished > DateTime.MinValue)
+            {
+                return "Live";
+            }
+
+            return ticket.CurrentActivity.Title;
         }
 
         private static string MapActivityTitle(string title)
@@ -95,6 +123,54 @@ namespace LeanKit.ReleaseManager.Controllers
 
     public class TicketViewModel
     {
+        public ActivityBreakdown ActivityBreakdown { get; set; }
 
+        public string Title { get; set; }
+
+        public string ExternalId { get; set; }
+
+        public string CurrentStatus { get; set; }
+
+        public string Started { get; set; }
+
+        public string Finished { get; set; }
+    }
+
+    public class ActivityBreakdown
+    {
+        public IEnumerable<ActivityBreakdownItem> Activities { get; private set; }
+
+        public ActivityBreakdown(IEnumerable<ActivityBreakdownItem> activities)
+        {
+            Activities = activities;
+        }
+    }
+
+    public class ActivityBreakdownItem
+    {
+        public string Activity { get; set; }
+
+        public string TimeSummary
+        {
+            get
+            {
+                return string.Format("{0} hr{1}", Hours, Hours != 1 ? "s" : "");
+            }
+        }
+
+        public int Days { get; set; }
+
+        public int Hours { get; set; }
+
+        public double Percent { get; set; }
+
+        public string FormattedPercent { get { return Percent.ToString("0.00"); } }
+
+        public bool IsWaste { get; set; }
+
+        public string ClassName
+        {
+            get { return Activity.Replace(" ", string.Empty); }
+        }
     }
 }
