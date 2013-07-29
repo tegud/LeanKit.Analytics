bcce8e128ec3d49f79721ac62320287616f633d0
Steve Elliott <steve@tegud.net>
2013-07-29 22:29:14 +0100

    Releasing dashboard prototype

---
+++ LeanKit.Analytics/LeanKit.ReleaseManager/Controllers/ReleaseDashboardController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeanKit.ReleaseManager.Controllers
+{
+    public class ReleaseDashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Status(string server)
+        {
+            var url = string.Format("http://{0}.tlrg.org/en/loadbalancercheck.mvc", server);
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.Method = "GET";
+            request.Timeout = 2500;
+
+            try
+            {
+                using (var response = request.GetResponse())
+                {
+                    var version = response.Headers["X-Version"];
+
+                    return Json(new
+                        {
+                            Status = "Online",
+                            Version = version
+                        }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError)
+                {
+                    var httpWebException = (HttpWebResponse) ex.Response;
+                    if ((int) httpWebException.StatusCode == 418)
+                        return Json(new
+                        {
+                            Status = "Deploying"
+                        }, JsonRequestBehavior.AllowGet);
+
+                    return Json(new
+                    {
+                        Status = "Down"
+                    }, JsonRequestBehavior.AllowGet);
+                }
+
+                if (ex.Status == WebExceptionStatus.Timeout)
+                {
+                    return Json(new
+                    {
+                        Status = "Timeout"
+                    }, JsonRequestBehavior.AllowGet);
+                }
+
+                return Json(new
+                {
+                    Status = "Error"
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ LeanKit.Analytics/RabbitMQTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMQTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                }
+            }
+        }
+    }
+}
