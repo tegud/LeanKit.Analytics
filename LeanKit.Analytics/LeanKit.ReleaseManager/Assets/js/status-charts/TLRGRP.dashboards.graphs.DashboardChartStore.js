8d108fcc17155b9af9de937069bf4c6a5472ef4a
Steve Elliott <steve@tegud.net>
2013-08-09 17:29:47 +0100

    Start/Stop graphs control added

--- LeanKit.Analytics/LeanKit.ReleaseManager/Assets/js/status-charts/TLRGRP.dashboards.graphs.DashboardChartStore.js
+++ LeanKit.Analytics/LeanKit.ReleaseManager/Assets/js/status-charts/TLRGRP.dashboards.graphs.DashboardChartStore.js
@@ -7,48 +7,59 @@ TLRGRP.namespace('TLRGRP.dashboards.graphs');
 			evaluator: ':1081/1.0/metric?expression=',
 			server: 'http://172.31.168.140'
 		};
-	
-	TLRGRP.dashboards.graphs.DashboardChartStore = function (options) {
-		var expressions,
-			chart;
-		
-		options = $.extend({}, defaultOptions, options);
-		
-		expressions = options.expressions;
-		chart = options.chart;
-		
-		function getData() {
-			var deferreds = [];
-			var datasetcount = expressions.length;
-			var evaluator = options.evaluator;
-			var server = options.server;
-			var retrievedData = {};
-			
-			for(n = 0; n < datasetcount; n++) {
-				(function(expression) {
-					var deferred = deferreds[deferreds.length] = $.Deferred();
 
-					d3.json(server + evaluator + expression.expression, function(data) {
-						data.forEach(function(d) {
-							d.time = new Date(d.time);
-							d.value = + d.value;
-						});
-						
-						retrievedData[expression.id] = data;
-						deferred.resolve();
-					});
-				})(expressions[n]);
-			};
-			
-			$.when.apply(this, deferreds).then(function() {
-				chart.drawChart(retrievedData);
-				
-				if(options.refreshRate) {
-					setTimeout(getData, options.refreshRate);
-				}
-			});
-		}
-		
-		getData();
-	}
+    TLRGRP.dashboards.graphs.DashboardChartStore = function(options) {
+        var expressions,
+            chart,
+            timeout;
+
+        options = $.extend({}, defaultOptions, options);
+
+        expressions = options.expressions;
+        chart = options.chart;
+
+        function getData() {
+            var deferreds = [];
+            var datasetcount = expressions.length;
+            var evaluator = options.evaluator;
+            var server = options.server;
+            var retrievedData = {};
+
+            for (n = 0; n < datasetcount; n++) {
+                (function(expression) {
+                    var deferred = deferreds[deferreds.length] = $.Deferred();
+
+                    d3.json(server + evaluator + expression.expression, function(data) {
+                        data.forEach(function(d) {
+                            d.time = new Date(d.time);
+                            d.value = +d.value;
+                        });
+
+                        retrievedData[expression.id] = data;
+                        deferred.resolve();
+                    });
+                })(expressions[n]);
+            }
+            ;
+
+            $.when.apply(this, deferreds).then(function() {
+                chart.drawChart(retrievedData);
+
+                if (options.refreshRate) {
+                    timeout = setTimeout(getData, options.refreshRate);
+                }
+            });
+        }
+
+        getData();
+
+        return {
+            start: function () {
+                getData();
+            },
+            stop: function() {
+                clearTimeout(timeout);
+            }
+        };
+    };
 })();
