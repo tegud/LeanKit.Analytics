393e029e7ff4ca9b015740c641e07c09de132ac2
Steve Elliott <steve@tegud.net>
2013-09-04 00:03:45 +0100

    Moving IIS metrics to separate file

--- LeanKit.Analytics/LeanKit.ReleaseManager/Assets/js/status/TLRGRP.BADGER.IIS.js
+++ LeanKit.Analytics/LeanKit.ReleaseManager/Assets/js/status/TLRGRP.BADGER.IIS.js
@@ -2,23 +2,122 @@
     TLRGRP.namespace('TLRGRP.BADGER.WMI');
 
     TLRGRP.BADGER.IIS = (function () {
+        function iisExpressionBuilder() {
+            return (function () {
+                var currentTimitSelectDataString = TLRGRP.BADGER.Cube.convertTimePeriod(currentTimePeriod);
+                return new TLRGRP.BADGER.Cube.ExpressionBuilder('lr_web_request').setTimePeriod(currentTimitSelectDataString);
+            });
+        }
+
+        var iis = iisExpressionBuilder();
+
         var allMetrics = {
-            'AllRequests': {
-                name: 'Requests',
-                expression: 'sum(lr_web_request)',
+            'TrafficByType': {
+                title: 'Traffic by Type',
+                expressions: [{
+                        title: 'All',
+                        expression: iis().sum()
+                    },
+                    {
+                        title: 'Bot',
+                        expression: iis().sum().equalTo('isbot', true)
+                    },
+                    {
+                        title: 'Mobile',
+                        expression: iis().sum().equalTo('isbot', false).equalTo('ismobile', true)
+                    }],
                 chartOptions: {
-                    lockToZero: true,
                     yAxisLabel: 'requests'
+                }
+            },
+            'ResponseTimeByPage': {
+                title: 'Response Time by Page',
+                expressions: [{
+                        title: 'Home Page',
+                        expression: iis().median('duration').equalTo('pagetype', 'home-page')
+                    },
+                    {
+                        title: 'Search',
+                        expression: iis().median('duration').equalTo('pagetype', 'search')
+                    },
+                    {
+                        title: 'Hotel Details',
+                        expression: iis().median('duration').equalTo('pagetype', 'hotel-details')
+                    },
+                    {
+                        title: 'Booking Form',
+                        expression: iis().median('duration').equalTo('pagetype', 'booking-form')
+                    }],
+                chartOptions: {
+                    yAxisLabel: 'request time (ms)'
+                }
+            },
+            'StatusCodes': {
+                title: 'Status Codes (non 200)',
+                expressions: [{
+                        title: '404 (Not Found)',
+                        expression: iis().sum().equalTo('status', 404)
+                    }, {
+                        title: '500 (Error)',
+                        expression: iis().sum().equalTo('status', 500)
+                    }, {
+                        title: '30x (Redirect)',
+                        expression: iis().sum().in('status', [301, 302])
+                    }],
+                chartOptions: {
+                    yAxisLabel: 'requests'
+                }
+            },
+            'TrafficByChannel': {
+                title: 'Traffic by Channel',
+                expressions: [{
+                        title: 'Direct',
+                        expression: iis().sum().equalTo('pagechannel', 'web')
+                    },
+                    {
+                        title: 'Mobile',
+                        expression: iis().sum().equalTo('pagechannel', 'mobile')
+                    },
+                    {
+                        title: 'Affiliate',
+                        expression: iis().sum().equalTo('pagechannel', 'affiliate')
+                    }],
+                chartOptions: {
+                    yAxisLabel: 'requests'
+                }
+            },
+            'TrafficByPage': {
+                title: 'Traffic by Page',
+                expressions: [{
+                        title: 'Home Page',
+                        expression: iis().sum().equalTo('pagetype', 'home-page')
+                    },
+                    {
+                        title: 'Search',
+                        expression: iis().sum().equalTo('pagetype', 'search')
+                    },
+                    {
+                        title: 'Hotel Details',
+                        expression: iis().sum().equalTo('pagetype', 'hotel-details')
+                    },
+                    {
+                        title: 'Booking Form',
+                        expression: iis().sum().equalTo('pagetype', 'booking-form')
+                    }],
+                chartOptions: {
+                    yAxisLabel: 'requests'
+                }
+            },
+            'IPGResponseTime': {
+                title: 'Average Time for Tokenisation',
+                expressions: [{
+                    expression: iis().sum().matchesRegEx('url', '/beacon/pageresponse')
+                }],
+                chartOptions: {
+                    yAxisLabel: 'duration (ms)'
                 }
             }
         };
-        
-        //{
-        //    id: 'iis-all',
-        //    title: 'All',
-        //    color: colors[0],
-        //    expression: 'sum(lr_web_request)&' + currentTimitSelectDataString
-        //}
         
         return {
             metricInfo: function (metricName) {
